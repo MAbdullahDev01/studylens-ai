6e8b3355fc95ff268881870344587fc354cf4d00
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-01-03 02:20:20 +0500

    feat: setup routing and create main pages - Added React Router v6 - Created HomePage, LoginPage, RegisterPage, Dashboard placeholders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Dashboard from "./pages/Dashboard";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <p>StudyLens AI</p>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
